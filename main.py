63cd9a2d4d87eb66b55587f8601cf52003d3106b
Dariazhuravleva <darya.spring@mail.ru>
2024-01-15 15:12:41 +0400

    debugging CRUD

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from post_service import app
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    app.run(port=5001, debug=True)
     
--- post_service/api/profile/posts.py
+++ post_service/api/profile/posts.py
@@ -20,86 +20,48 @@ def get_all_posts():
     }, 200
 
 
-# @api.post("/new_posts")
-# def create_new_posts():
-#     title = request.json.get("title")
-#     body = request.json.get("body")
-#     if not title or not body:
-#         return {
-#             "status": 1,
-#             "description": "Fail",
-#             "data": {}
-#         }, 400
-#     check_name = Posts.query.filter_by(title=title).first()
-#     if check_name:
-#         return {
-#             "status": 2,
-#             "description": "Title already exist",
-#             "data": {}
-#             }, 400
-#     item = Posts(title=title, body=body)
-#     db.session.add(item)
-#     db.session.commit()
-#     return {
-#         "status": 0,
-#         "description": "OK",
-#         "data": {
-#             "posts": {
-#                 "id": item.id,
-#                 "title": item.title,
-#                 "body": item.body
-#             }
-#         }
-#     }, 200
+@api.post("/new_posts")
+def create_new_posts():
+    title = request.json.get("title")
+    body = request.json.get("body")
+    if not title or not body:
+        return {
+            "status": 1,
+            "description": "Fail",
+            "data": {}
+        }, 400
+    responce = requests.post(f"http://{os.getenv('DB_SERVICE_HOST')}:{os.getenv('DB_SERVICE_PORT')}/new_posts",
+                             json=request.json, headers={"Content-Type": "application/json"})
+    return {
+        "status": 0,
+        "description": "OK",
+        "data": {
+            "posts": responce.json().get("posts")
+        }
+    }, 200
     
     
-# @api.put("/posts/<int:id>")
-# def update_posts(id):
-#     item = Posts.query.get(id)
-#     if not item:
-#         return {
-#             "status": 3,
-#             "description": "Fail",
-#             "data": {}
-#         }, 400
-#     title = request.json.get("title")
-#     body = request.json.get("body")
-#     item.title = title if title else item.title
-#     item.body = body if body else item.body
-#     db.session.commit()
-#     return {
-#         "status": 0,
-#         "description": "OK",
-#         "data": {
-#             "posts": {
-#                 "id": item.id,
-#                 "title": item.title,
-#                 "body": item.body
-#             }
-#         }
-#     }, 200
+@api.put("/posts/<int:id>")
+def update_posts(id):
+    responce = requests.put(f"http://{os.getenv('DB_SERVICE_HOST')}:{os.getenv('DB_SERVICE_PORT')}/posts/{id}",
+                            json=request.json, headers={"Content-Type": "application/json"})
+    return {
+        "status": 0,
+        "description": "OK",
+        "data": {
+            "posts": responce.json().get("posts")
+        }
+    }, 200
     
-
-# @api.delete("/delete_posts/<int:id>")
-# def delete_posts(id):
-#     item = Posts.query.get(id)
-#     if not item:
-#         return {
-#             "status": 3,
-#             "description": "Fail",
-#             "data": {}
-#         }, 400
-#     db.session.delete(item)
-#     db.session.commit()
-#     return {
-#         "status": 0,
-#         "description": "OK",
-#         "data": {
-#             "posts": {
-#                 "id": item.id,
-#                 "title": item.title,
-#                 "body": item.body
-#             }
-#         }
-#     }, 200
     
+@api.delete("/delete_posts/<int:id>")
+def delete_posts(id):
+    responce = requests.delete(f"http://{os.getenv('DB_SERVICE_HOST')}:{os.getenv('DB_SERVICE_PORT')}/delete_posts/{id}",
+                            json=request.json, headers={"Content-Type": "application/json"})
+    return {
+        "status": 0,
+        "description": "OK",
+        "data": {
+            "posts": responce.json().get("posts")
+        }
+    }, 200
